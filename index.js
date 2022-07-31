ea665b367db4dd7b4e32d31dd398ee8efb737a6e
Denes Beck <denes.beck@hu.ibm.com>
2022-07-31 16:52:15 +0200

    package updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
